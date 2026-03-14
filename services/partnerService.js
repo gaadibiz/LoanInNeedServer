4bb408dea007e87ff22ea8e5ea1c5c415818e8b1
pro123sub <pro123sub@gmail.com>
2026-03-14 13:30:36 +0530

    fixed server crash 😃

--- services/partnerService.js
+++ services/partnerService.js
@@ -5,11 +5,6 @@ const { generateHmac, encrypt, decrypt } = require('../utils/cryptoUtils');
 const crypto = require('crypto');
 const { BadRequestError, NotFoundError, UnauthorizedError } = require('../GlobalExceptionHandler/exception');
 const smsOtpService = require('../utils/smsOtpService');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const { generateHmac, encrypt, decrypt } = require('../utils/cryptoUtils');
-const crypto = require('crypto');
-const { BadRequestError, NotFoundError, UnauthorizedError } = require('../GlobalExceptionHandler/exception');
 
 const API_BASE_URL = process.env.API_BASE_URL || 'http://localhost:5000';
 
