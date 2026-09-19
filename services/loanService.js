d2d98b34e3af4e3350852e26f3f4840a01770e2d
monika@edgenroots.net <monika@monikas-MacBook-Air.local>
2026-09-19 11:17:13 +0530

    Fix : add eAadhaar fetch at and other documnets

--- services/loanService.js
+++ services/loanService.js
@@ -290,6 +290,7 @@ async function sendLoanApplicationToBumchum(userId, applicationId = '',) {
             date_of_visit: new Date().toISOString(),
             cancellation_dead_reason: application.reason || '',
             ipQualityDetail: ipQualityDetail,
+            IPStatus: ipQualityDetail ? (String(ipQualityDetail.recentAbuse) === 'true' || Number(ipQualityDetail.fraudScore)) > 0 ? 'F' : 'P' : 'N/A',
             employment_type_uuid: employeeDetail?.employmentType === 'SALARIED' ? 'e54e543d-a20e-47b5-8bf1-a087e910d92b' : '3d9d2e30-2754-49f8-be31-3a14c1d720b7',
             action_item_category_uuid: 'f22bd31f-b9cb-4c8e-a07b-50f9b7083812',
             ...utm
